a1156836f89d4cdb6f6e1d9b7d07778e69f64cf1
edwinyzhang <edwinyzhang@tencent.com>
2021-03-18 23:19:38 +0800

    update consul register code

--- monolic/monolic.py
+++ monolic/monolic.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, make_response
+import requests
 import json
 
 app = Flask(__name__)
@@ -24,6 +25,19 @@ def check_health():
     return json.dumps({'success': True}), 200, \
          {'ContentType': 'application/json'}
 
+@app.route('/service_register')
+def service_register():
+    url = "http://127.0.0.1:8500/v1/agent/service/register?replace-existing-checks=true"
+
+    payload={  "ID": "monolic2",  "Name": "monolic4",  "Tags": [ "v1"],  "Address": "127.0.0.1",   "Port": 8080 }
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("PUT", url, headers=headers, data=json.dumps(payload))
+
+    return str(response.status_code)
+    
 
 def get_1():
     return 1
